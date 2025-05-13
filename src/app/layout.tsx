60d716a02edfe77ec55220e38c0010a9124ebf85
slydragonn <alogo18m@gmail.com>
2025-05-12 20:30:53 -0500

    feat: add clerk authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,16 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 import './globals.css';
+import Link from 'next/link';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -23,8 +33,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <header className='flex justify-around items-center p-4 gap-4 h-16'>
+            <Link href='/'>BuhoPrompt</Link>
+            <div className='flex gap-4'>
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
-import prisma from '@/lib/db/prisma';
-
 export default async function Home() {
-  const templates = await prisma.template.findMany();
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <h1>Test Prisma</h1>
-      <div>
-        <h2>Templates descriptions</h2>
-        <ul>
-          {templates.map((template) => (
-            <li key={template.id}>{template.description}</li>
-          ))}
-        </ul>
-      </div>
+      <h1>BuhoPrompt</h1>
     </div>
   );
 }
